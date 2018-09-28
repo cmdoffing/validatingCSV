4afc0c68e1adbd9ff0e18458123c5bb148f03597
Mike Doffing <c.m.doffing@gmail.com>
2018-09-27 20:18:42 -0400

    most of README
    full readme functionality except empty validation fields

--- vcsvTest.py
+++ vcsvTest.py
@@ -8,23 +8,26 @@ readerParams = {'delimiter': '|'}
 def description_is_invalid(field):
     return 'Description is too long' if len(field) > 15 else None
 
-def price_converter(field):
+def price_converter(field, base=10):
     return round(float(field))
+
+def year_converter(field, base):
+    return int(field, base)
 
 
 year_validation   = {'name': 'year', 'desc': 'The year the car was made.',
-                     'type': int}
+                     'type': 'integer', 'converter': int, 'base': 10, 'min': 1999, 'max': 2020}
 make_validation   = {'name': 'make', 'valid_values': ['Ford', 'Chevy', 'Jeep']}
 model_validation  = {'name': 'model'}
-desc_validation   = {'name': 'description', 'error_checker': description_is_invalid}
-price_validation  = {'name': 'price', 'type': float, 'converter': price_converter}
-validation_params = (year_validation, make_validation, model_validation, desc_validation, price_validation)
+desc_validation   = {'name': 'description', 'error_checker': description_is_invalid,
+                     'default': '***'}
+price_validation  = {'name': 'price', 'type': 'float', 'converter': price_converter}
+validation_params = (year_validation, make_validation, None, desc_validation, price_validation)
 
 
 if __name__ == '__main__':
     filepath = sys.argv[1]
-    with open(filepath) as csvfile:
-        rdr = vcsv.ValidatingCSVReader(csvfile, readerParams, validation_params)
-        for row in rdr:
-            if row:
-                print(row)
+    rdr = vcsv.ValidatingCSVReader(filepath, readerParams, validation_params)
+    for row in rdr:
+        if row:
+            print(row)
