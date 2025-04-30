0e721181704180f1076b88f8aae5cd075bb16472
Michael Doffing <43443159+cmdoffing@users.noreply.github.com>
2025-04-30 16:23:09 -0400

    Update validatingCSV.py

--- validatingCSV.py
+++ validatingCSV.py
@@ -285,3 +285,18 @@ class ValidatingCSVReader:
         if max_len and len(value) > max_len:
             return 'The length of value "' + field + '" is greater than max_len'
         return
+
+import sys
+if __name__ == '__main__':
+    csv_file_path    = sys.argv[1]
+    json_params_path = sys.argv[2]
+    reader = validatingCSV.ValidatingCSVReader(csv_file_path, json_params_path,
+                                               error_file_path='./test/temp.errors',
+                                               log_file_path='vcsv.log')
+    for row in reader:
+        if row:
+            print(row)
+
+    print('\n---------- Errors -----------')
+    for err in reader.errors:
+        print(err)
